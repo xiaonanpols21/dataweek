51c7990d683e38304335147e217f7cd605977ced
xiaonanpols21 <xiaonanpolspols@gmail.com>
2022-12-05 21:44:02 +0100

    data gereed gemaakt

--- scripts/main.js
+++ scripts/main.js
@@ -7,4 +7,36 @@ Promise.all(urls.map(u => fetch(u)))
   .then(responses => Promise.all(responses.map((res) => res.json())))
   .then(data => {
     console.log(data);
+
+    /* 
+    ********** 0 = BTS, 1 = Harry Styles ***********
+    */
+
+    // Intro
+    const intro = []
+    data.forEach(item => {
+        intro.push(item.data.artist.profile.biography.text);
+    });
+    console.log(intro)
+
+    // Top tracks
+    const tracks = []
+    data.forEach(item => {
+        tracks.push(item.data.artist.discography.topTracks.items);
+    });
+    console.log(tracks)
+
+    // Monthly listeners
+    const listeners = []
+    data.forEach(item => {
+        listeners.push(item.data.artist.stats.monthlyListeners);
+    });
+    console.log(listeners);
+
+    // Top 5 cities
+    const city = []
+    data.forEach(item => {
+        city.push(item.data.artist.stats.topCities.items);
+    });
+    console.log(city);
   });
