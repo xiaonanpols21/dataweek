b8dd8e25c449a8558e7664ccad24665b42c74efc
xiaonanpols21 <xiaonanpolspols@gmail.com>
2022-12-06 08:45:32 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
 import '../styles/style.css'
 import * as d3 from 'd3';
 
+// Fetch data
 const urls = ["./data/overview-bts.json", "./data/overview-hs.json"];
-
 Promise.all(urls.map(u => fetch(u)))
   .then(responses => Promise.all(responses.map((res) => res.json())))
   .then(data => {
